470a6f818df5b3a37174104f91c0de5fdbca5a5d
temesgen <temesgenmulugeta11@gmail.com>
2025-01-04 21:58:25 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
-import { Calendar2 } from "@/components/custom-date-picker";
-import DatePicker from "@/components/date-picker";
-import DateRangePicker from "@/components/date-range-picker";
+
+import EthiopianCalendar from "@/components/custom-date-picker";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <DatePicker />
-        <DateRangePicker />
-        <Calendar2 />
-    
+        <EthiopianCalendar />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
--- src/components/custom-date-picker.tsx
+++ src/components/custom-date-picker.tsx
@@ -1,230 +1,26 @@
 "use client";
 
-import { Button, buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { EthiopianDate } from "@/util/ethiopian-date-utils";
-import {
-  CustomCalendarMonth,
-  getCalendarMonth,
-} from "@/utils/CustomCalendarMonth";
 import { EthiopianDateLib } from "@/utils/EthiopianDateLib";
-import { differenceInCalendarDays } from "date-fns";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import * as React from "react";
-import {
-  DateLibOptions,
-  DayPicker,
-  labelNext,
-  labelPrevious,
-  Month,
-  MonthGrid,
-  UI,
-  useDayPicker,
-  type DayPickerProps,
-} from "react-day-picker";
+import { DateLibOptions, DayPicker } from "react-day-picker";
 
-export type CalendarProps = DayPickerProps & {
-  /**
-   * In the year view, the number of years to display at once.
-   * @default 12
-   */
-  yearRange?: number;
-
-  /**
-   * Wether to show the year switcher in the caption.
-   * @default true
-   */
-  showYearSwitcher?: boolean;
-
-  monthsClassName?: string;
-  monthCaptionClassName?: string;
-  weekdaysClassName?: string;
-  weekdayClassName?: string;
-  monthClassName?: string;
-  captionClassName?: string;
-  captionLabelClassName?: string;
-  buttonNextClassName?: string;
-  buttonPreviousClassName?: string;
-  navClassName?: string;
-  monthGridClassName?: string;
-  weekClassName?: string;
-  dayClassName?: string;
-  dayButtonClassName?: string;
-  rangeStartClassName?: string;
-  rangeEndClassName?: string;
-  selectedClassName?: string;
-  todayClassName?: string;
-  outsideClassName?: string;
-  disabledClassName?: string;
-  rangeMiddleClassName?: string;
-  hiddenClassName?: string;
-};
-
-/**
- * A custom calendar component built on top of react-day-picker.
- * @param props The props for the calendar.
- * @default yearRange 12
- * @returns
- */
-function Calendar2({
-  className,
-  showOutsideDays = true,
-  showYearSwitcher = true,
-  yearRange = 12,
-  numberOfMonths,
-  ...props
-}: CalendarProps) {
-  const [navView, setNavView] = React.useState<"days" | "years">("days");
-  const [displayYears, setDisplayYears] = React.useState<{
-    from: number;
-    to: number;
-  }>(
-    React.useMemo(() => {
-      const currentYear = new Date().getFullYear();
-      return {
-        from: currentYear - Math.floor(yearRange / 2 - 1),
-        to: currentYear + Math.ceil(yearRange / 2),
-      };
-    }, [yearRange])
-  );
-
-  const { onNextClick, onPrevClick, startMonth, endMonth } = props;
-
-  const columnsDisplayed = navView === "years" ? 1 : numberOfMonths;
-
-  const _monthsClassName = cn("relative flex", props.monthsClassName);
-  const _monthCaptionClassName = cn(
-    "relative mx-10 flex h-7 items-center justify-center",
-    props.monthCaptionClassName
-  );
-  const _weekdaysClassName = cn("flex flex-row", props.weekdaysClassName);
-  const _weekdayClassName = cn(
-    "w-8 text-sm font-normal text-muted-foreground",
-    props.weekdayClassName
-  );
-  const _monthClassName = cn("w-full", props.monthClassName);
-  const _captionClassName = cn(
-    "relative flex items-center justify-center pt-1",
-    props.captionClassName
-  );
-  const _captionLabelClassName = cn(
-    "truncate text-sm font-medium",
-    props.captionLabelClassName
-  );
-  const buttonNavClassName = buttonVariants({
-    variant: "outline",
-    className:
-      "absolute h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
-  });
-  const _buttonNextClassName = cn(
-    buttonNavClassName,
-    "right-0",
-    props.buttonNextClassName
-  );
-  const _buttonPreviousClassName = cn(
-    buttonNavClassName,
-    "left-0",
-    props.buttonPreviousClassName
-  );
-  const _navClassName = cn("flex items-start", props.navClassName);
-  const _monthGridClassName = cn("mx-auto mt-4", props.monthGridClassName);
-  const _weekClassName = cn("mt-2 flex w-max items-start", props.weekClassName);
-  const _dayClassName = cn(
-    "flex size-8 flex-1 items-center justify-center p-0 text-sm",
-    props.dayClassName
-  );
-  const _dayButtonClassName = cn(
-    buttonVariants({ variant: "ghost" }),
-    "size-8 rounded-md p-0 font-normal transition-none aria-selected:opacity-100",
-    props.dayButtonClassName
-  );
-  const buttonRangeClassName =
-    "bg-accent [&>button]:bg-primary [&>button]:text-primary-foreground [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground";
-  const _rangeStartClassName = cn(
-    buttonRangeClassName,
-    "day-range-start rounded-s-md",
-    props.rangeStartClassName
-  );
-  const _rangeEndClassName = cn(
-    buttonRangeClassName,
-    "day-range-end rounded-e-md",
-    props.rangeEndClassName
-  );
-  const _rangeMiddleClassName = cn(
-    "bg-accent !text-foreground [&>button]:bg-transparent [&>button]:!text-foreground [&>button]:hover:bg-transparent [&>button]:hover:!text-foreground",
-    props.rangeMiddleClassName
-  );
-  const _selectedClassName = cn(
-    "[&>button]:bg-primary [&>button]:text-primary-foreground [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground",
-    props.selectedClassName
-  );
-  const _todayClassName = cn(
-    "[&>button]:bg-accent [&>button]:text-accent-foreground",
-    props.todayClassName
-  );
-  const _outsideClassName = cn(
-    "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
-    props.outsideClassName
-  );
-  const _disabledClassName = cn(
-    "text-muted-foreground opacity-50",
-    props.disabledClassName
-  );
-  const _hiddenClassName = cn("invisible flex-1", props.hiddenClassName);
-  const dateLib = getDateLib({
-    weekStartsOn: 0,
-  });
-
+export default function EthiopianCalendar() {
+  const dateLib = getDateLib();
   return (
     <DayPicker
-      showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
-    //   style={{
-    //     width: 248.8 * (columnsDisplayed ?? 1) + "px",
-    //   }}
       components={{
         Day(props) {
-    
           return <td {...props}>{props.children}</td>;
         },
-
         Week(props) {
-          //   console.log("props.week", props.week);
           return <tr {...props}>{props.children}</tr>;
         },
       }}
-      classNames={{
-        months: _monthsClassName,
-        month_caption: _monthCaptionClassName,
-        weekdays: _weekdaysClassName,
-        weekday: _weekdayClassName,
-        month: "text-red-500",
-        caption: _captionClassName,
-        caption_label: _captionLabelClassName,
-        button_next: _buttonNextClassName,
-        button_previous: _buttonPreviousClassName,
-        nav: _navClassName,
-        month_grid: _monthGridClassName,
-        week: _weekClassName,
-        day: _dayClassName,
-        day_button: _dayButtonClassName,
-        range_start: _rangeStartClassName,
-        range_middle: _rangeMiddleClassName,
-        range_end: _rangeEndClassName,
-        selected: _selectedClassName,
-        today: _todayClassName,
-        outside: _outsideClassName,
-        disabled: _disabledClassName,
-        hidden: _hiddenClassName,
-      }}
       dateLib={dateLib}
-      {...props}
+    
     />
   );
 }
-Calendar2.displayName = "Calendar";
 
-export { Calendar2 as Calendar2 };
 
 export const getDateLib = (options?: DateLibOptions) => {
   return new EthiopianDateLib({
